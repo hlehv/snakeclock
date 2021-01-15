dae34d939f214b7ce34d6e271ed6e1e7316e65d3
Hollis Lehv <hlehv@aol.com>
2021-01-14 19:23:56 -0500

    Finished snake clock

--- SnakeClock/sketch.js
+++ SnakeClock/sketch.js
@@ -11,35 +11,45 @@ let YMAX;
 let XMIN;
 let YMIN;
 
-/*let XMAXmin;
+let XMAXmin;
 let XMINmin;
 let YMINmin;
-let YMAXmin;*/
+let YMAXmin;
+
+let XMAXsec;
+let XMINsec;
+let YMINsec;
+let YMAXsec;
 
 let SQUARESIZE;
 
 let currminute;
 
 function setup() {
-	createCanvas(1300,900); // make an HTML canvas element width x height pixels
+	createCanvas(657,500); // make an HTML canvas element width x height pixels
 
-	XMAX = width - 30;
-	XMIN = 20;
-	YMIN = 80;
+	XMAX = width - 20;
+	XMIN = 10;
+	YMIN = 20;
 	YMAX = height - 30;
 
-	/*XMAXmin = width - 50;
+	XMAXmin = width - 40;
 	XMINmin = 50;
 	YMINmin = 120;
-	YMAXmin = height - 80;*/
+	YMAXmin = height - 80;
+
+	XMAXsec = width - 100;
+	XMINsec = 100;
+	YMINsec = 180;
+	YMAXsec = height - 100;
 
 	xhour = XMIN;
-	xmin = XMIN;
-	xsec = XMIN;
+	xmin = XMINmin;
+	xsec = XMINsec;
 
 	yhour = YMIN;
-	ymin = YMIN;
-	ysec = YMIN;
+	ymin = YMINmin;
+	ysec = YMINsec;
 
 	SQUARESIZE = 17;
 
@@ -56,8 +66,10 @@ function draw() {
 	fill(0);
 	text(second(), 10, 90);*/
 
-	/*
+	
 	//hour snake clock
+	fill('rgb(0,235,255)');
+	stroke('rgb(0,235,255)');
 	let lastposx = xhour;
 	let lastposy = yhour;
 	for (let i = 0; i < hour(); i++)
@@ -68,7 +80,7 @@ function draw() {
 		{
 			xpos = lastposx - SQUARESIZE - 5;
 			ypos = YMIN;
-		} else if (lastposx >= (XMAX - SQUARESIZE - 5) && lastposy <= YMAX + 1)
+		} else if (lastposx >= (XMAX - SQUARESIZE - 5) && lastposy <= YMAX + 1 && lastposy > YMIN)
 		{
 			xpos = XMAX;
 			ypos = lastposy - SQUARESIZE - 5;
@@ -88,21 +100,21 @@ function draw() {
 
 	if (xhour < XMAX && yhour <= YMIN)
 	{
-		xhour += 8;
+		xhour += 1;
 		//square(xhour, YMIN, SQUARESIZE);
 	} else if (xhour >= XMAX && yhour < YMAX)
 	{
-		yhour += 8;
+		yhour += 1;
 		//square (XMAX, yhour, SQUARESIZE);
 	} else if (xhour > XMIN && yhour >= YMAX)
 	{
-		xhour -= 8;
+		xhour -= 1;
 		//square (xhour, YMAX, SQUARESIZE);
 	} else if (xhour <= XMIN && yhour > YMIN)
 	{
-		yhour -= 8;
+		yhour -= 1;
 		//square(XMIN, yhour, SQUARESIZE);
-	}*/
+	}
 
 	if (currminute != minute())
 	{
@@ -111,26 +123,30 @@ function draw() {
 	}
 
 	//minute snake clock
-	let lastposx = xmin;
-	let lastposy = ymin;
-	for (let i = 0; i < hour(); i++)
+
+	fill('rgb(255,0,245)');
+	stroke('rgb(255,0,245)');
+
+	lastposx = xmin;
+	lastposy = ymin;
+	for (let i = 0; i < minute(); i++)
 	{
 		let xpos;
 		let ypos;
-		if (lastposx <= XMAX + 1 && lastposy <= (YMIN + SQUARESIZE + 5) && lastposx > (XMIN + SQUARESIZE + 5))
+		if (lastposx <= XMAXmin + 1 && lastposy <= (YMINmin + SQUARESIZE + 5) && lastposx > (XMINmin + SQUARESIZE + 5))
 		{
 			xpos = lastposx - SQUARESIZE - 5;
-			ypos = YMIN;
-		} else if (lastposx >= (XMAX - SQUARESIZE - 5) && lastposy <= YMAX + 1)
+			ypos = YMINmin;
+		} else if (lastposx >= (XMAXmin - SQUARESIZE - 5) && lastposy <= YMAXmin + 1)
 		{
-			xpos = XMAX;
+			xpos = XMAXmin;
 			ypos = lastposy - SQUARESIZE - 5;
-		} else if (lastposy >= (YMAX - SQUARESIZE - 5) && lastposx >= XMIN)
+		} else if (lastposy >= (YMAXmin - SQUARESIZE - 5) && lastposx >= XMINmin)
 		{
 			xpos = lastposx + SQUARESIZE + 5;
-			ypos = YMAX;
+			ypos = YMAXmin;
 		} else {
-			xpos = XMIN;
+			xpos = XMINmin;
 			ypos = lastposy + SQUARESIZE + 5;
 		}
 
@@ -139,24 +155,65 @@ function draw() {
 		lastposy = ypos;
 	}
 
-	if (xmin < XMAX && ymin <= YMIN)
+	if (xmin < XMAXmin && ymin <= YMINmin)
 	{
-		xmin += 8;
-		//square(xhour, YMIN, SQUARESIZE);
-	} else if (xmin >= XMAX && ymin < YMAX)
+		xmin += 4;
+	} else if (xmin >= XMAXmin && ymin < YMAXmin)
 	{
-		ymin += 8;
-		//square (XMAX, yhour, SQUARESIZE);
-	} else if (xmin > XMIN && ymin >= YMAX)
+		ymin += 4;
+	} else if (xmin > XMINmin && ymin >= YMAXmin)
 	{
-		xmin -= 8;
-		//square (xhour, YMAX, SQUARESIZE);
-	} else if (xmin <= XMIN && ymin > YMIN)
+		xmin -= 4;
+	} else if (xmin <= XMINmin && ymin > YMINmin)
 	{
-		ymin -= 8;
-		//square(XMIN, yhour, SQUARESIZE);
+		ymin -= 4;
 	}
 
+	//second snake clock
+	fill('rgb(0,255,0)');
+	stroke('rgb(0,255,0)');
+
+	lastposx = xsec;
+	lastposy = ysec;
+	for (let i = 0; i < second(); i++)
+	{
+		let xpos;
+		let ypos;
+		if (lastposx <= XMAXsec + 1 && lastposy <= (YMINsec + SQUARESIZE + 6) && lastposx > (XMINsec + SQUARESIZE + 6))
+		{
+			xpos = lastposx - SQUARESIZE - 5;
+			ypos = YMINsec;
+		} else if (lastposx >= (XMAXsec - SQUARESIZE - 4) && lastposy <= YMAXsec + 1)
+		{
+			xpos = XMAXsec;
+			ypos = lastposy - SQUARESIZE - 5;
+		} else if (lastposy >= (YMAXsec - SQUARESIZE - 4) && lastposx >= XMINsec)
+		{
+			xpos = lastposx + SQUARESIZE + 5;
+			ypos = YMAXsec;
+		} else {
+			xpos = XMINsec;
+			ypos = lastposy + SQUARESIZE + 5;
+		}
+
+		square (xpos, ypos, SQUARESIZE);
+		lastposx = xpos;
+		lastposy = ypos;
+	}
+
+	if (xsec < XMAXsec && ysec <= YMINsec)
+	{
+		xsec += 8;
+	} else if (xsec >= XMAXsec && ysec < YMAXsec)
+	{
+		ysec += 8;
+	} else if (xsec > XMINsec && ysec >= YMAXsec)
+	{
+		xsec -= 8;
+	} else if (xsec <= XMINsec && ysec > YMINsec)
+	{
+		ysec -= 8;
+	}
 	/*
 	//create the hour snake
 	fill('rgb(0,255,0)');
